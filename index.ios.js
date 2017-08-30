5ee909ae10ad3dd6fad938fa01f47465006c93ee
Kyle Johnson <kyle@solidstategroup.com>
2017-08-30 11:04:01 +0100

    Revert to old rn

--- index.ios.js
+++ index.ios.js
@@ -2,40 +2,5 @@
 
 import { NativeModules } from 'react-native';
 
-const RNCalendarEvents = NativeModules.RNCalendarEvents
-
-export default {
-
-  authorizationStatus () {
-    return RNCalendarEvents.authorizationStatus()
-  },
-
-  authorizeEventStore () {
-    return RNCalendarEvents.authorizeEventStore()
-  },
-
-  fetchAllEvents (startDate, endDate, calendars = []) {
-    return RNCalendarEvents.fetchAllEvents(startDate, endDate, calendars)
-  },
-
-  findCalendars () {
-    return RNCalendarEvents.findCalendars();
-  },
-
-  findEventById (id) {
-    return RNCalendarEvents.findEventById(id);
-  },
-
-  saveEvent (title, details) {
-    return RNCalendarEvents.saveEvent(title, details)
-  },
-
-  removeEvent (id) {
-    return RNCalendarEvents.removeEvent(id)
-  },
-
-  removeFutureEvents (id) {
-    return RNCalendarEvents.removeFutureEvents(id)
-  }
-
-}
+var RNCalendarEvents = NativeModules.RNCalendarEvents;
+export default RNCalendarEvents;
